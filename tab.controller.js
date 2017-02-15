04c06d261a19ddbae0ea2bd49a57cc7eaf2f3156
Jam <pjdsilvestre@tmj.ph>
2017-02-15 11:01:30 +0800

    Added boilerplate

---
+++ tab.controller.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+    angular
+        .module('tab')
+        .controller('TabController', TabController);
+    TabController.$inject = [];
+    /* @ngInject */
+    function TabController() {
+        var vm = this;
+        vm.title = 'Controller';
+        activate();
+        
+        function activate() {
+            console.log(vm.title);
+        }
+    }
+})();
